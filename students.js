eee01b36ee1ad35865ca5fff24d5e2344cd80f33
Woranan Hernandez <126374724+WH220405@users.noreply.github.com>
2023-03-16 20:24:21 -0700

    added my info.

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Woranan',
+        lastName:'Hernandez',
+        location: 'California, CA',
+        html:'worananHernandez.html',
+        imagePath:'images/worananHernandez.jpg'
     },
 ]
 
