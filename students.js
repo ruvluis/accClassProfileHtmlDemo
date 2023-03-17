42ad696892fb49bfa93e868238b5f2645655364b
Nathan Sobarnia <126843191+Jynxful@users.noreply.github.com>
2023-03-16 19:02:14 -0700

    added my name

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Nathan',
+        lastName:'Sobarnia',
+        location: 'California, CA',
+        html:'nathansobarnia.html',
+        imagePath:'images/nathanSobarnia.jpg'
     },
 ]
 
