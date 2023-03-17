f6608b5b9223627f73210436ce4f2e176193a145
MarlonDowdell <126838031+MarlonDowdell@users.noreply.github.com>
2023-03-16 20:25:18 -0700

    Update students.js
    updated my information

--- students.js
+++ students.js
@@ -11,28 +11,8 @@ export const data = [
         firstName:'Marlon',
         lastName:'Dowdell',
         location: 'Riverside CA',
-        html:'marlondowdell.html',
-        imagePath:'images/marlondowdell.jpg'
-    },
-    {
-        firstName:'Mich',
-        lastName:'Jimenez',
-        location: 'Redlands CA',
-        html:'michaelJimenez.html',
-        imagePath:'images/michaelJimenez.jpg'
-    },{
-        firstName:'Teke',
-        lastName:'Byas',
-        location: 'Rancho Cucamonga, CA',
-        html:'tekeByas.html',
-        imagePath:'images/tekeByas.jpg'
-     },{  
-
-        firstName:'Bonnie',
-        lastName:'Lee',
-        location: 'Fontana, CA',
-        html:'bonnieLee.html',
-        imagePath:'images/bonnieLee.jpg'
-    },
+        html:'marlonDowdell.html',
+        imagePath:'images/marlonDowdell.jpg'
+    }
 ]
 
