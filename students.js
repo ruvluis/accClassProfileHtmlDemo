2a3aae09d8acf0567a5f98978226bdea65e588bc
Soloukdar1355 <102251279+Soloukdar1355@users.noreply.github.com>
2023-03-16 19:06:25 -0700

    Update students.js
    I added my profile

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Alireza',
+        lastName:'Soloukdar',
+        location: 'California, CA',
+        html:'alirezaSoloukdar.html',
+        imagePath:'images/alirezaSoloukdar.jpg'
     },
 ]
 
