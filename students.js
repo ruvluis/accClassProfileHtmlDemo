7c45e07a88a7760a7be496d761548babb1f9dd68
iwonalafave <126426658+iwonalafave@users.noreply.github.com>
2023-03-16 19:02:44 -0700

    Added my info

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Iwona',
+        lastName:'LaFave',
+        location: 'Redlands, Ca',
+        html:'iwonalafave.html',
+        imagePath:'images/iwonaLafave.jpg'
     },
 ]
 
