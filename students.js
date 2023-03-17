55e7a984dc9ccbd342dd8ee044faaf3dd901988a
klurpz <126437401+klurpz@users.noreply.github.com>
2023-03-16 19:17:16 -0700

    updated my profile

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:'Michael',
+        lastName:'Jimenez',
+        location: 'Redlands CA',
+        html:'michaelJimenez.html',
+        imagePath:'images/michaelJimenez.jpg'
     },
 ]
 
