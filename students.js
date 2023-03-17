6813094092ea390999a746e1c5e29d3fbc765faa
ruvluis <126355510+ruvluis@users.noreply.github.com>
2023-03-16 19:09:21 -0700

    Update students.js
    new branch

--- students.js
+++ students.js
@@ -7,11 +7,11 @@ export const data = [
         imagePath:'images/davidLiang.jpg'
     },
     {
-        firstName:'David',
-        lastName:'Liang',
-        location: 'Philadephia, PA',
-        html:'davidLiang.html',
-        imagePath:'images/davidLiang.jpg'
+        firstName:Luis',
+        lastName:'Ruvalcaba',
+        location: 'Rialto,CA',
+        html:'luisruv.html',
+        imagePath:'images/luisruv.jpg'
     },
 ]
 
